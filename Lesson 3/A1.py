d087f4b18504af932ece6637ca2cfb2892d78f77
IndianHim <ryansach007@gmail.com>
2026-07-07 14:48:50 -0700

    L3A1

--- Lesson 3/A1.py
+++ Lesson 3/A1.py
@@ -1,2 +1,17 @@
-3 = 3 * 7
-print(3)
+
+# 1) Store an integer value in `a`.
+a = 10
+# 2) Print the datatype of `a` using `type()`.
+print(type(a))
+# 3) Store a decimal (float) value in `b`.
+b=7.07
+# 4) Print the datatype of `b` using `type()`.
+print(type(b))
+# 5) Store a text (string) value in `c`.
+c="Hello"
+# 6) Print the datatype of `c` using `type()`.
+print(type(c))
+# 7) Store a boolean value (True/False) in `d`.
+d= True
+# 8) Print the datatype of `d` using `type()`.
+print(type(d))
