8c9bd508e341b3e3e610f675fe0de1497fada82e
IndianHim <ryansach007@gmail.com>
2026-07-10 14:42:26 -0700

    A2

--- Lesson 3/A2.py
+++ Lesson 3/A2.py
@@ -11,17 +11,20 @@ weight = 7.07
 # 2) Print each variable’s value.
 print(name, age, is_student, weight)
 # 3) Print the datatype of each variable using `type()`.
-type(name)
-type(weight)
-type(age)
-type(is_student)
+print(type(name))
+print(type(weight))
+print(type(age))
+print(type(is_student))
 # 4) Show a message that type casting will happen next.
 print("We will be typecasting the variables")
 # 5) Convert `age` from an integer to a string and store it back in `age`.
 age = str(age)
 # 6) Print `age` and print its datatype again to confirm it changed.
-print(age) type(age)
+print(age) 
+print(type(age))
 # 7) Convert `weight` from a float to an integer and store it back in `weight`.
 weight = int(weight)
 # 8) Print `weight` and print its datatype again to confirm it changed.
-print(weight), type(weight)
+print(weight)
+print(type(weight))
+      
