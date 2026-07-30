7b78af1e7a3221dea736bf3cc39a9a0574d44d27
IndianHim <ryansach007@gmail.com>
2026-07-30 13:53:38 -0700

    L2HW

---
+++ Lesson 2/L2HW.py
@@ -0,0 +1,10 @@
+print("==== App Access Operator ====")
+requested_app = input("What app would you like to access? The coding app, the math app, or the history app?")
+if requested_app == "coding app" :
+    print("You have reached our coding app. Let your creativity flowout through the language of python. =)")
+elif requested_app == "math app" :
+    print ("You have reached our math app. Now go solve those tricky word problems.") 
+elif requested_app == "history app" :
+    print ("You have reached our history app. Now prepare to be amazed by our historic artifacts that tell a story throughout the ages.")
+else :
+    print("Please enter a valid app you would like to enter. Thank you.")    
